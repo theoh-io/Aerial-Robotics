599668b15d5c8311ff4693f404701cba3096087d
Amaelle Bidet <amaelle.bidet@epfl.ch>
2022-04-30 12:19:10 +0200

    First tests hovering and position command + tutorial sbs_motion_commander.md

---
+++ motion_flying.py
@@ -0,0 +1,104 @@
+import logging
+import sys
+import time
+from threading import Event
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E714')
+
+DEFAULT_HEIGHT = 0.5
+BOX_LIMIT = 0.5
+
+deck_attached_event = Event()
+
+logging.basicConfig(level=logging.ERROR)
+
+position_estimate = [0, 0]
+
+def param_deck_flow(_, value_str):
+    value = int(value_str) #conversion str to int
+    print(value)
+    if value:
+        deck_attached_event.set()
+        print('Deck is attached!')
+    else:
+        print('Deck is NOT attached!')
+
+def move_linear_simple(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        time.sleep(1)
+        mc.forward(0.5)
+
+        #or mc.back()
+        time.sleep(1)
+        mc.turn_left(180)
+        time.sleep(1)
+        mc.forward(0.5)
+        time.sleep(1)
+
+def log_pos_callback(timestamp, data, logconf):
+    print(data)
+    global position_estimate
+    position_estimate[0] = data['stateEstimate.x']
+    position_estimate[1] = data['stateEstimate.y']
+
+def take_off_simple(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        #mc.up(0.3) # to go even higher then the default height or change default height
+        time.sleep(3)
+        mc.stop()
+
+def move_box_limit(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        
+        body_x_cmd = 0.2
+        body_y_cmd = 0.1
+        max_vel = 0.2
+
+        while (1):
+            if position_estimate[0] > BOX_LIMIT:
+                 body_x_cmd=-max_vel
+            elif position_estimate[0] < -BOX_LIMIT:
+                body_x_cmd=max_vel
+            if position_estimate[1] > BOX_LIMIT:
+                body_y_cmd=-max_vel
+            elif position_estimate[1] < -BOX_LIMIT:
+                body_y_cmd=max_vel
+
+            mc.start_linear_motion(body_x_cmd, body_y_cmd, 0)
+
+            time.sleep(0.1)
+
+if __name__ == '__main__':
+
+    cflib.crtp.init_drivers()
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+
+        #want to know if the flow deck is correctly attached before flying,
+        scf.cf.param.add_update_callback(group="deck", name="bcFlow2",
+                                         cb=param_deck_flow)
+        time.sleep(1)
+        #or
+        if not deck_attached_event.wait(timeout=5):
+            print('No flow deck detected!')
+            sys.exit(1)
+
+        logconf = LogConfig(name='Position', period_in_ms=10)
+        logconf.add_variable('stateEstimate.x', 'float')
+        logconf.add_variable('stateEstimate.y', 'float')
+        scf.cf.log.add_config(logconf)
+        logconf.data_received_cb.add_callback(log_pos_callback)
+
+        #start logging
+        logconf.start()
+
+        move_box_limit(scf)
+
+        #stop logging
+        logconf.stop()
