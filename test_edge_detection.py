672446373e09659496df4b25a5364a8ae1ff4f55
cguillau <camille.guillaume@epfl.ch>
2022-05-08 22:33:03 +0200

    Modif edge_detect

--- test_edge_detection.py
+++ test_edge_detection.py
@@ -7,7 +7,6 @@ Created on Sun May  8 16:52:23 2022
 """
 
 import logging
-import sys
 import time
 
 import cflib.crtp
@@ -49,39 +48,39 @@ if __name__ == '__main__':
     cf = Crazyflie(rw_cache='./cache')
     with SyncCrazyflie(URI, cf=cf) as scf:
         with MotionCommander(scf) as mc:
-            with PositionHlCommander(scf, default_velocity=0.2, default_height=1, controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
-                with Multiranger(scf) as multiranger:
-            
-                    time.sleep(4)
+            #with PositionHlCommander(scf, default_velocity=0.2, default_height=1, controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
+            with Multiranger(scf) as multiranger:
         
-                    # Go to a coordinate and use default height
-                    pc.go_to(0.0, 0.0)
-        
-                    # Go to a coordinate
-                    #pc.go_to(1.0, 1.0, 1.0)
-                    # Go slowly to a coordinate
-                    #pc.go_to(1.0, 1.0, velocity=0.2)
-        
-                    # Set new default velocity and height
-                    #pc.set_default_velocity(0.3)
-                    #pc.set_default_height(1.0)
-                    #pc.go_to(0.0, 0.0)
+                time.sleep(4)
+    
+                # Go to a coordinate and use default height
+                #pc.go_to(0.0, 0.0)
+    
+                # Go to a coordinate
+                #pc.go_to(1.0, 1.0, 1.0)
+                # Go slowly to a coordinate
+                #pc.go_to(1.0, 1.0, velocity=0.2)
+    
+                # Set new default velocity and height
+                #pc.set_default_velocity(0.3)
+                #pc.set_default_height(1.0)
+                #pc.go_to(0.0, 0.0)
+                
+                keep_flying = True
+
+                mc.up(1)
+                
+                while keep_flying:
                     
-                    keep_flying = True
+                    z_1 = multiranger.down
+                    time.sleep(0.1)
+                    z_2 = multiranger.down
                     
-                    while keep_flying:
-                        
-                        z_1 = multiranger.down
-                        time.sleep(0.1)
-                        z_2 = multiranger.down
-                        
-                        if is_edge(z_1,z_2):
-                            pc.go_to(0.0, 0.0, 0.0)
-                        
-                        if is_close(multiranger.up):
-                            keep_flying = False
-                        if is_close(multiranger.down):
-                            keep_flying = False
-                        
-                        time.sleep(0.1)
+                    if is_edge(z_1,z_2):
+                        mc.stop()
+                    
+                    if is_close(multiranger.up):
+                        keep_flying = False
+                    
+                    time.sleep(0.1)
         
