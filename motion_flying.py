9fd33fbed961961846442393ae9774c946634122
cguillau <camille.guillaume@epfl.ch>
2022-05-22 16:07:28 +0200

    Add edge detection and logs to mention_flying

--- motion_flying.py
+++ motion_flying.py
@@ -3,7 +3,12 @@ from pickle import FALSE
 import sys
 import time
 from threading import Event
+from threading import Timer
 import math
+import datetime as dt
+from zipfile import ZIP_BZIP2
+import numpy as np
+import os
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
@@ -11,14 +16,16 @@ from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
-from cflib.utils.multiranger import Multiranger
+from cflib.utils.multiranger import Multiranger  
+from cflib.crazyflie.syncLogger import SyncLogger
+from cflib.positioning.position_hl_commander import PositionHlCommander
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E714')
 
 # Unit: meter
-DEFAULT_HEIGHT = 1 #1
+DEFAULT_HEIGHT = 0.5 #1
 FOV_ZRANGER=math.radians(2.1)
-BOX_LIMIT_X = 2 #5
+BOX_LIMIT_X = 1.5 #5
 BOX_LIMIT_Y = 0.5 #3
 START_POS_X = 0
 START_POS_Y = 0
@@ -39,9 +46,16 @@ len_x, len_y = (500, 200)
 
 deck_attached_event = Event()
 
+# Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
 
+# Logs global variables
 position_estimate = [0, 0, 0, 0]
+logs = np.zeros([100000,4])
+count = 0
+
+# Edge detection global variables
+edge = False
 
 def param_deck_flow(_, value_str):
     value = int(value_str) #conversion str to int
@@ -51,6 +65,39 @@ def param_deck_flow(_, value_str):
         print('Deck is attached!')
     else:
         print('Deck is NOT attached!')
+
+# Logs functions -------------------------------------------------------------------------------------------
+def log_pos_callback(timestamp, data, logconf):
+    #print(data)
+    global position_estimate
+    position_estimate[0] = data['stateEstimate.x']
+    position_estimate[1] = data['stateEstimate.y']
+    position_estimate[2] = data['stateEstimate.z']
+    position_estimate[3] = data['range.zrange']
+
+def stab_log_data(timestamp, data, logconf):
+    """Callback froma the log API when data arrives"""
+    #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+    global count
+    
+    # Save info into log variable
+    for idx, i in enumerate(list(data)):
+        if idx < 3:
+            logs[count][idx] = data[i]*1000
+        else:
+            logs[count][idx] = data[i]
+    count += 1
+
+def store_log_data():
+    # Get timestamp
+    filename = dt.datetime.now().strftime("%Y_%m_%d_%H_%M_%S.csv")
+    # Save log to file
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    filepath = os.path.join(os.getcwd(),'logs',filename)
+    np.savetxt(filepath, logs, delimiter=',')
+
+# -------------------------------------------------------------------------------------------------------------
 
 def move_linear_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
@@ -63,14 +110,6 @@ def move_linear_simple(scf):
         time.sleep(1)
         mc.forward(0.5)
         time.sleep(1)
-
-def log_pos_callback(timestamp, data, logconf):
-    #print(data)
-    global position_estimate
-    position_estimate[0] = data['stateEstimate.x']
-    position_estimate[1] = data['stateEstimate.y']
-    position_estimate[2] = data['stateEstimate.z']
-    position_estimate[3] = data['stateEstimate.yaw']
 
 
 def take_off_simple(scf):
@@ -124,7 +163,11 @@ def zigzag_nonblocking():
         case=state_zigzag["back2left"]
     #Temporaire: condition d'arret si la limite de l'arene en x 
     if position_estimate[0] > BOX_LIMIT_X - START_POS_X:
+        print("Limite arene x reached")
         mc.land()
+        time.sleep(1)
+        case =state_zigzag["arrived"]
+    
     #Temporaire condition de retour basé sur le temps de vol
     if(time.time()-start_time>TIME_EXPLORE):
         print(" Exploration time exceeded")
@@ -135,7 +178,19 @@ def zigzag_nonblocking():
         goal_y=position_estimate[1]
         mc.land()
         time.sleep(1)
-        mc.take_off(DEFAULT_HEIGHT)
+        #mc.take_off(DEFAULT_HEIGHT)
+        #clean_takeoff(mc, [goal_x, goal_y, yaw_landing])
+        case =state_zigzag["arrived"] #to get out of zigzag
+
+    if ( edge == True and (case != state_zigzag["start"]) and (case != state_zigzag["arrived"]) ):
+        print('Edge detected!')
+        yaw_landing=position_estimate[2]
+        #must record goal pos before landing because variation can occur
+        goal_x=position_estimate[0]
+        goal_y=position_estimate[1]
+        mc.land()
+        time.sleep(1)
+        #mc.take_off(DEFAULT_HEIGHT)
         #clean_takeoff(mc, [goal_x, goal_y, yaw_landing])
         case =state_zigzag["arrived"] #to get out of zigzag
 
@@ -338,6 +393,33 @@ def obstacle_avoidance():
     return False
 
 
+# Edge detection functions ---------------------------------------------------------------------------------
+def is_edge_2():
+    MIN_EDGE2 = 60  # mm
+    logs_copy2=logs[~np.all(logs == 0, axis=1)]
+
+    if len(logs_copy2) > 80:
+        #z_2=logs_copy2[-1,3]
+        #z_1=logs_copy2[-50,3]
+        
+        z_2=np.max(logs_copy2[-70:,3])
+        idx_2=np.argmax(logs_copy2[-70:,3])
+        z_1=np.min(logs_copy2[-70:,3])
+        idx_1=np.argmax(logs_copy2[-70:,3])
+
+        if abs(z_1-z_2) > MIN_EDGE2:
+            print('abs edge: ',abs(z_1-z_2))
+            print('idx 1: ',idx_1)
+            print('idx 2: ',idx_2)
+            print(idx_2)
+            return True
+        else:
+            return False
+    else:
+        return False
+
+# ------------------------------------------------------------------------------------------------------------
+
 if __name__ == '__main__':
 
     cflib.crtp.init_drivers()
@@ -356,9 +438,10 @@ if __name__ == '__main__':
         logconf.add_variable('stateEstimate.x', 'float')
         logconf.add_variable('stateEstimate.y', 'float')
         logconf.add_variable('stateEstimate.z', 'float')
-        logconf.add_variable('stateEstimate.yaw', 'float')
+        logconf.add_variable('range.zrange', 'uint16_t')
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(log_pos_callback)
+        logconf.data_received_cb.add_callback(stab_log_data)
 
         #start logging
         logconf.start()
@@ -394,16 +477,21 @@ if __name__ == '__main__':
 
 
                 while(1):
-                    print(obstacle_avoidance())
-                    if (obstacle_avoidance() == False):
+                    #print(obstacle_avoidance())
+                    #if (obstacle_avoidance() == False):
+                    if True:
                         print('obs false')
                         #if no obstacle is being detected let zigzag manage the speeds
                         if case != state_zigzag['arrived']:
+                            edge = is_edge_2()
                             zigzag_nonblocking()
                         else:
-                            regulate_yaw(mc, yaw_landing, position_estimate[3]) #compensate the error in yaw during landing
-                            print("yaw after regulate:", position_estimate[3])
-                            go_back()
+                            #regulate_yaw(mc, yaw_landing, position_estimate[3]) #compensate the error in yaw during landing
+                            #print("yaw after regulate:", position_estimate[3])
+                            #go_back()
+                            logconf.stop()
+                            store_log_data()
+                            break
                         time.sleep(1)
                     else:
                         print("obstacle av = True")
@@ -412,6 +500,7 @@ if __name__ == '__main__':
                         mc.start_linear_motion(velocity_x, velocity_y, 0)
                         time.sleep(0.1)
         #stop logging
-        logconf.stop()
+        #logconf.stop()
+        #store_log_data()
 
         
